cd3cd7dff594ddfacb7ea1176c6b4302bef2d648
ivanklak <ivanklak17@gmail.com>
2020-11-20 19:41:24 +0100

    Online shop

---
+++ app.js
@@ -0,0 +1,5 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCarBtn = document.querySelector(".close-cart");
+const closeCarBtn = document.querySelector(".close-cart");
